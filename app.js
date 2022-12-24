f9d64cedf2b6c82af7fd4df3a482b5c01e451909
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-24 11:48:55 +0530

    added error handling

--- app.js
+++ app.js
@@ -61,6 +61,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err
+      }
+      const data = err.originalError.data
+      const message = err.message || 'An Error Occured!'
+      const code = err.originalError.code || 500
+      return { message: message, status: code, data: data }
+    },
   }),
 )
 
