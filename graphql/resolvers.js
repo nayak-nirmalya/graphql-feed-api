9818e6fc7df4a4b741f47ffe587793d8023c4080
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-24 11:39:23 +0530

    added validator npm package and validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,28 @@
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 const User = require('../models/user')
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
+    const errors = []
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({
+        message: 'Invalid E-Mail!',
+      })
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({
+        message: 'Password is too short!',
+      })
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid Input!')
+      throw error
+    }
     const existingUser = await User.findOne({ email: userInput.email })
     if (existingUser) {
       const error = new Error('User Already Exists!')
